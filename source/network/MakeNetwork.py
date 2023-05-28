b7690c6aa6a266661ed7b5a64c0b4471443918ac
tkck110213 <penetrate.wonder@gmail.com>
2023-05-28 15:43:51 +0900

    fix word2vec model path(convert ~ to home folder)

--- source/network/MakeNetwork.py
+++ source/network/MakeNetwork.py
@@ -3,12 +3,14 @@ from gensim.models import KeyedVectors
 import re
 from tqdm import tqdm
 import sys
-
+import os
 
 class SimilarNetwork:
     def __init__(self):
+        path = f'{os.path.expanduser("~")}/Project/Resource/jawiki.all_vectors.200d.txt'
+        print(f"model path:{path}")
         print("Word2Vec loading...")
-        self.model = KeyedVectors.load_word2vec_format("~/Project/Resource/jawiki.all_vectors.200d.txt")
+        self.model = KeyedVectors.load_word2vec_format(path)
         print("Complete Word2Vec loading")
 
         self.wordlist = self.model.index_to_key
@@ -19,7 +21,7 @@ class SimilarNetwork:
             similarWords = self.model.most_similar(positive=[word], topn=k)
             self.similarityNetwork.add_weighted_edges_from([(re.sub("##", "", word), re.sub("##", "", similar[0]), similar[1]) for similar in similarWords])
 
-        nx.write_gml(self.similarityNetwork, f"~/Project/Resource/SimilarityNetwork-k{k}.gml")
+        nx.write_gml(self.similarityNetwork, f'{os.path.expanduser("~")}/Project/Resource/SimilarityNetwork-k{k}.gml')
         
 
 
